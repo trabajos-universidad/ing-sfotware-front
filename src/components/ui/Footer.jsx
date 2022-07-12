90c96947818e2a143cebedd16125b62396204e34
Chino <victort8012@gmail.com>
2022-07-12 15:37:37 -0400

    [Fix] problemas de ui en dispositivos moviles

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div className="footer mt-5">
       <div className="row">
-        <div className="col-12">
+        <div className="col">
           <img
             src="./imgs/logo.png"
             alt="logo"
@@ -11,14 +11,14 @@ export const Footer = () => {
         </div>
       </div>
       <div className="row mt-3">
-        <div className="col-12 line">
+        <div className="col line">
           <p className="text-white icons">
             <a href="https://www.instagram.com/artymad.pyme/" target="_blank" className="fa-brands fa-instagram size mt-1 mx-3"></a>
             <a className="fa-brands fa-whatsapp size mt-1 mx-3"></a>
           </p>
         </div>
         <div className="row">
-          <div className="col-12">
+          <div className="col">
             <p className="text-white icons">
               © 2020 Todos los derechos reservados.
             </p>
