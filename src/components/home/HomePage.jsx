b982036d747954348f044b762ec5dca880188232
Chino <victort8012@gmail.com>
2022-07-13 21:21:16 -0400

    finalinzando la pagina de home

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -17,16 +17,16 @@ export const HomePage = () => {
             </div>
           </li>
           <li className="paso mt-3">
-            <img src="./imgs/madera.jpg" alt="imagen" className="paso-img"/> 
+            <img src="./imgs/home2.jpg" alt="imagen" className="paso-img"/> 
             <div>
               <h3 className="text-white">Paso 2</h3>
-              <p className="text-white"> Primero se toma un tronco de madera el cual cumpla con los estandares de calidad</p>
+              <p className="text-white"> Luego se comienza con la manufacturacion del producto donde se trabaja directamente con la maquinaria</p>
             </div>
           </li><li className="paso mt-3">
-            <img src="./imgs/madera.jpg" alt="imagen" className="paso-img"/> 
+            <img src="./imgs/producto.jpg" alt="imagen" className="paso-img"/> 
             <div>
               <h3 className="text-white">Paso 3</h3>
-              <p className="text-white"> Primero se toma un tronco de madera el cual cumpla con los estandares de calidad</p>
+              <p className="text-white"> Finalmente se termina de pulir el producto para entregarselo al cliente</p>
             </div>
           </li>
         </ul>
